20a0053510e6039b6d64f9dd1d2254d1c070ba71
Mathias Wagner <germannewsmaker@gmail.com>
2026-04-15 17:22:08 +0200

    Create Servers page

---
+++ client/src/pages/Servers/Servers.jsx
@@ -0,0 +1,49 @@
+import "./styles.sass";
+import Icon from "@mdi/react";
+import { mdiPlus, mdiServerOutline } from "@mdi/js";
+import AllServers from "./pages/AllServers";
+import { useState } from "react";
+import Button from "@/common/components/Button";
+import AddServerDialog from "./components/AddServerDialog";
+
+export const Servers = () => {
+    const [showAddDialog, setShowAddDialog] = useState(false);
+    const [refreshKey, setRefreshKey] = useState(0);
+
+    const handleServerCreated = () => {
+        setRefreshKey(prev => prev + 1);
+    };
+    
+    return (
+        <div className="servers-page">
+            <div className="servers-header">
+                <div className="header-content">
+                    <div className="header-icon">
+                        <Icon path={mdiServerOutline} />
+                    </div>
+                    <div className="header-text">
+                        <h1>Servers</h1>
+                        <p>Manage your infrastructure</p>
+                    </div>
+                </div>
+                <div className="header-actions">
+                    <Button 
+                        text="Add Server" 
+                        icon={mdiPlus}
+                        onClick={() => setShowAddDialog(true)}
+                    />
+                </div>
+            </div>
+
+            <div className="servers-content">
+                <AllServers key={refreshKey} />
+            </div>
+
+            <AddServerDialog 
+                open={showAddDialog} 
+                onClose={() => setShowAddDialog(false)}
+                onServerCreated={handleServerCreated}
+            />
+        </div>
+    )
+}
