fa0fc78660e48f148e71f460ea5b772d01aed533
Mathias Wagner <germannewsmaker@gmail.com>
2026-04-15 19:04:49 +0200

    Fix button styling

--- client/src/common/components/Button/Button.jsx
+++ client/src/common/components/Button/Button.jsx
@@ -5,7 +5,7 @@ export const Button = ({onClick, text, icon, disabled, type, buttonType}) => {
     return (
         <button className={"btn" + (type ? " type-" + type : "")} onClick={onClick} disabled={disabled} type={buttonType}>
             {icon ? <Icon path={icon} /> : null}
-            <h3>{text}</h3>
+            <span>{text}</span>
         </button>
     );
 }
